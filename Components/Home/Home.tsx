0f5b3477b85e5e037ddd2f516d56ff35495e723e
tae100k <tkim3@oswego.edu>
2022-09-04 15:45:01 +0900

    refactor: make templogin component

--- Components/Home/Home.tsx
+++ Components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { MOCK_CARD_LIST, POPULAR_BEER_TITLE } from "../../Static";
+import TempLogin from "../Auth/TempLogin";
 import CardList from "./CardList";
 import SearchBarHome from "./SearchBarHome";
 import TwoByTwoCard from "./TwoByTwoCard";
@@ -8,45 +9,24 @@ import WelcomeText from "./WelcomeText";
 
 const HomeComponent = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userNickname, setuserNickname] = useState("");
+  const [userNickname, setUserNickname] = useState("");
 
-  const handleKeypress = (e: React.KeyboardEvent) => {
+  const handleLogin = () => {
     setIsLoggedIn(!isLoggedIn);
+  };
+
+  const handleUserName = (newUserName: string) => {
+    setUserNickname(newUserName);
   };
 
   return (
     <Container>
-      {isLoggedIn ? (
-        <>
-          <span>{`${userNickname}님 로그인 됨`}</span>
-          <button
-            onClick={() => {
-              setIsLoggedIn(!isLoggedIn);
-              setuserNickname("");
-            }}
-          >
-            로그아웃 하기
-          </button>
-        </>
-      ) : (
-        <form>
-          <input
-            onChange={(e) => {
-              setuserNickname(e.target.value);
-            }}
-            value={userNickname}
-          />
-          <button
-            onKeyPress={handleKeypress}
-            onClick={() => {
-              setIsLoggedIn(!isLoggedIn);
-            }}
-          >
-            로그인하기
-          </button>
-        </form>
-      )}
-
+      <TempLogin
+        handleLogin={handleLogin}
+        handleUserName={handleUserName}
+        isLoggedIn={isLoggedIn}
+        userNickname={userNickname}
+      />
       <WelcomeText nickname={userNickname} isLoggedIn={isLoggedIn} />
       <SearchBarHome />
       {isLoggedIn ? (
