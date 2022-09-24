132e8511fbf040049db31a47580f68bfbf1cf3ba
tae100k <tkim3@oswego.edu>
2022-09-24 20:40:11 +0900

    feat: x button added, social login button added

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,13 +1,18 @@
-import { Center } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 import ContinueButton from "../../Components/Auth/Continue";
 import SocialButton from "../../Components/Auth/SocialButton";
+import Title from "../../Components/Auth/Title";
+import { X } from "../../public/svg";
 
 const LoginTemplate = () => {
   return (
-    <Center h="calc(100vh)" gap="10px" flexDirection="column" px="20px">
-      <SocialButton />
-      <ContinueButton />
-    </Center>
+    <>
+      <Title />
+      <Center h="calc(100vh)" gap="10px" flexDirection="column" px="20px">
+        <SocialButton />
+        <ContinueButton />
+      </Center>
+    </>
   );
 };
 
--- public/svg/index.tsx
+++ public/svg/index.tsx
@@ -4,3 +4,4 @@ export { default as Kakao } from "./kakao.svg";
 export { default as Google } from "./google.svg";
 export { default as Naver } from "./naver.svg";
 export { default as None } from "./none.svg";
+export { default as X } from "./x.svg";
