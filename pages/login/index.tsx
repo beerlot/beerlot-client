507bd4b64b277c7f5e4c0f6472adb6014be41dfe
tae100k <tkim3@oswego.edu>
2022-09-24 20:43:43 +0900

    feat: marking text draft added

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,14 +1,15 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import ContinueButton from "../../Components/Auth/Continue";
+import MarketingText from "../../Components/Auth/MarketingText";
 import SocialButton from "../../Components/Auth/SocialButton";
 import Title from "../../Components/Auth/Title";
-import { X } from "../../public/svg";
 
 const LoginTemplate = () => {
   return (
     <>
       <Title />
       <Center h="calc(100vh)" gap="10px" flexDirection="column" px="20px">
+        <MarketingText />
         <SocialButton />
         <ContinueButton />
       </Center>
