febd0f916f5ec655c2ed11c8c875b20a11c53c75
tae100k <tkim3@oswego.edu>
2022-09-25 15:58:57 +0900

    feat: empty search result login added

--- Components/Search/SearchBarAutocomplete.tsx
+++ Components/Search/SearchBarAutocomplete.tsx
@@ -1,26 +1,57 @@
-import {
-  Box,
-  Button,
-  Circle,
-  Divider,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { SearchGlass, WhiteCross } from "../../public/svg";
-import { SEARCH_BAR_PLACEHOLDER } from "../../Static";
+import { useEffect, useMemo, useState } from "react";
+import { beerItemType } from "../../Static";
 import SearchInput from "./SearchInput";
 
 const SearchBarAutocomplete = () => {
   const [value, setValue] = useState("");
+  const MOCK_BEER_ITEMS = [
+    {
+      id: 0,
+      beerName: "블랑",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+    {
+      id: 1,
+      beerName: "블랑 1988",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+    {
+      id: 2,
+      beerName: "호가든",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+    {
+      id: 3,
+      beerName: "칭따오",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+    {
+      id: 4,
+      beerName: "오비라거",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+    {
+      id: 5,
+      beerName: "OB라거",
+      img_src: "https://picsum.photos/id/237/200/300",
+      sort: "IPA",
+      country: "🌎",
+    },
+  ];
+
+  const [selectedItems, setSelectedItems] = useState<beerItemType[]>([]);
   const router = useRouter();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
@@ -28,32 +59,18 @@ const SearchBarAutocomplete = () => {
     const selectedName = e.currentTarget.textContent;
     router.push(`/result/${selectedName}`);
   };
+
+  // MOCK_BEER_ITEM useMemo처리 안하면, VALUE한번 바꿀 때마다 mock_beer_info가 다시 RENDER됨.
+  useEffect(() => {
+    const newSelectedItems: beerItemType[] = MOCK_BEER_ITEMS.filter(
+      (beerItems) => beerItems.beerName.includes(value)
+    );
+    setSelectedItems(newSelectedItems);
+  }, [value]);
+
   const clearInput = () => {
     setValue("");
   };
-  const items = [
-    {
-      id: 0,
-      name: "츄로스 랜드",
-    },
-    {
-      id: 1,
-      name: "칠성사이다 제로",
-    },
-    {
-      id: 2,
-      name: "펩시 제로",
-    },
-    {
-      id: 3,
-      name: "맛소금",
-    },
-    {
-      id: 4,
-      name: "오예스 미니",
-    },
-    { id: 5, name: "제로" },
-  ];
 
   return (
     <Flex w="full" direction="column" borderRadius="20px" gap="10px" mt="14px">
@@ -64,18 +81,26 @@ const SearchBarAutocomplete = () => {
       />
 
       <Flex flexDirection="column">
-        {items.map((beerInfo) => {
-          if (beerInfo.name.includes(value)) {
-            return (
-              <Box borderBottom="1px solid #DDDDDD" py="20px" px="15px">
-                <Text textStyle="h2" key={beerInfo.id} onClick={handleClick}>
-                  {beerInfo.name}
-                </Text>
-              </Box>
-            );
-          }
-          return;
-        })}
+        <>
+          {selectedItems.length !== 0 ? (
+            selectedItems.map((beerItems) => {
+              return (
+                <Box
+                  borderBottom="1px solid #DDDDDD"
+                  py="20px"
+                  px="15px"
+                  key={beerItems.id}
+                >
+                  <Text textStyle="h2" key={beerItems.id} onClick={handleClick}>
+                    {beerItems.beerName}
+                  </Text>
+                </Box>
+              );
+            })
+          ) : (
+            <Text>Nothing exists</Text>
+          )}
+        </>
       </Flex>
     </Flex>
   );
