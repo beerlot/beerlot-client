5b60fd43b5ad441711866f19504cbb0fb4785921
tae100k <tkim3@oswego.edu>
2022-09-25 16:00:13 +0900

    feat: MOCK_BEER_ITEMS replaced

--- Components/Search/SearchBarAutocomplete.tsx
+++ Components/Search/SearchBarAutocomplete.tsx
@@ -1,56 +1,11 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
-import { beerItemType } from "../../Static";
+import { beerItemType, MOCK_CARD_LIST } from "../../Static";
 import SearchInput from "./SearchInput";
 
 const SearchBarAutocomplete = () => {
   const [value, setValue] = useState("");
-  const MOCK_BEER_ITEMS = [
-    {
-      id: 0,
-      beerName: "블랑",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-    {
-      id: 1,
-      beerName: "블랑 1988",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-    {
-      id: 2,
-      beerName: "호가든",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-    {
-      id: 3,
-      beerName: "칭따오",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-    {
-      id: 4,
-      beerName: "오비라거",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-    {
-      id: 5,
-      beerName: "OB라거",
-      img_src: "https://picsum.photos/id/237/200/300",
-      sort: "IPA",
-      country: "🌎",
-    },
-  ];
-
   const [selectedItems, setSelectedItems] = useState<beerItemType[]>([]);
   const router = useRouter();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -62,7 +17,7 @@ const SearchBarAutocomplete = () => {
 
   // MOCK_BEER_ITEM useMemo처리 안하면, VALUE한번 바꿀 때마다 mock_beer_info가 다시 RENDER됨.
   useEffect(() => {
-    const newSelectedItems: beerItemType[] = MOCK_BEER_ITEMS.filter(
+    const newSelectedItems: beerItemType[] = MOCK_CARD_LIST.filter(
       (beerItems) => beerItems.beerName.includes(value)
     );
     setSelectedItems(newSelectedItems);
