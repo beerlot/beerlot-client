9d1fce9a3bbf1d0e1d66a49d184ae37974c49bc2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-10-15 13:04:51 +0900

    feat: back button, circle added

---
+++ Components/Auth/SignUp/BeerCards.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BeerCards = () => {
+  return <div>BeerCards</div>;
+};
+
+export default BeerCards;
--- Components/Auth/SignUp/Beers.tsx
+++ Components/Auth/SignUp/Beers.tsx
@@ -1,7 +1,24 @@
+import { Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
+import BackButton from "../../Utils/BackButton";
+import CompleteCircles from "../../Utils/CompleteCircles";
+import BeerCards from "./BeerCards";
 
 const Beers = () => {
-  return <div>beers</div>;
+  const router = useRouter();
+  const onClick = () => {
+    router.back();
+  };
+  return (
+    <>
+      <Flex h="100vh" px="27.5px" pb="73px" pt="34px" flexDirection="column">
+        <BackButton onClick={onClick} />
+        <CompleteCircles isNicknameDone={true} isBeersDone={true} />
+        <BeerCards />
+      </Flex>
+    </>
+  );
 };
 
 export default Beers;
