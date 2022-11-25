458afb220104a30ac0e0500a82c46b6ea542454d
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-11-25 22:05:26 +0900

    feat: marketing text added, useQuery to call api added

--- src/search/SearchBarAutocomplete.tsx
+++ src/search/SearchBarAutocomplete.tsx
@@ -1,17 +1,37 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { debounce } from "lodash";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { beerItemType, MOCK_CARD_LIST } from "../../interface/static";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useMutation } from "react-query";
+import useKeyboard from "../../hooks/useKeyboard";
+import { useSearch } from "../../hooks/useSearch";
 import EmptySearchResult from "./EmptySearchResult";
 import SearchInput from "./SearchInput";
 
 const SearchBarAutocomplete = () => {
   const router = useRouter();
   const [value, setValue] = useState("");
-  const [selectedItems, setSelectedItems] = useState<beerItemType[]>([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const { isInputEmpty } = useSearch(value);
 
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const { isEnterKey } = useKeyboard();
+
+  const searchBeer = useMutation(
+    (keyword: string) =>
+      axios.get(
+        `/api/v1/beers?keyword=${keyword}&page=1&size=10&sort=MOST_LIKES`
+      ) // page, size 수정해야 함.
+  );
+
+  // console searchBeer
+  useEffect(() => {
+    console.log(searchBeer.data?.data.contents);
+  }, [searchBeer.data?.data.contents]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
+    searchBeer.mutate(value);
   };
 
   const handleClickItem = (e: React.MouseEvent) => {
@@ -23,53 +43,69 @@ const SearchBarAutocomplete = () => {
     setValue("");
   };
 
-  // https://beerlot-core-obtg3qwuhq-an.a.run.app/api/v1/beers?keyword=포터&page=1&size=10&sort=MOST_LIKES
-
-  // MOCK_BEER_ITEM useMemo처리 안하면, VALUE한번 바꿀 때마다 mock_beer_info가 다시 RENDER됨.
   useEffect(() => {
-    const newSelectedItems: beerItemType[] = MOCK_CARD_LIST.filter(
-      (beerItems) => beerItems.beerName.includes(value)
-    );
-    setSelectedItems(newSelectedItems);
-  }, [value]);
+    const newSelectedItem = searchBeer.data?.data.contents;
+    setSelectedItems(newSelectedItem);
+  }, [searchBeer.data?.data.contents]);
+
+  const onCheckEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isEnterKey(e)) {
+      router.push(`/result/${e.target.value}}`);
+    }
+  };
 
   return (
     <Flex w="full" direction="column" borderRadius="20px" gap="10px" mt="14px">
       <SearchInput
+        onKeyPress={onCheckEnter}
         value={value}
-        handleChange={handleChangeInput}
+        onChange={handleChange}
         clearInput={clearInput}
       />
-
-      <Flex flexDirection="column">
-        <>
-          {selectedItems.length > 0 ? (
-            selectedItems.map((beerItems) => {
-              return (
-                <Box
-                  borderBottom="1px solid"
-                  borderColor="gray.200"
-                  py="10px"
-                  px="15px"
-                  key={beerItems.id}
-                >
-                  <Text
-                    textStyle="h2"
+      {isInputEmpty ? (
+        <MarketingScreen />
+      ) : (
+        <Flex flexDirection="column">
+          <>
+            {selectedItems?.length > 0 ? (
+              selectedItems.map((beerItems: any) => {
+                return (
+                  <Box
+                    borderBottom="1px solid"
+                    borderColor="gray.200"
+                    py="10px"
+                    px="15px"
                     key={beerItems.id}
-                    onClick={handleClickItem}
                   >
-                    {beerItems.beerName}
-                  </Text>
-                </Box>
-              );
-            })
-          ) : (
-            <EmptySearchResult inputValue={value} />
-          )}
-        </>
-      </Flex>
+                    <Text
+                      textStyle="h2"
+                      key={beerItems.id}
+                      onClick={handleClickItem}
+                    >
+                      {beerItems.name_ko}
+                    </Text>
+                  </Box>
+                );
+              })
+            ) : (
+              <EmptySearchResult inputValue={value} />
+            )}
+          </>
+        </Flex>
+      )}
     </Flex>
   );
 };
 
 export default SearchBarAutocomplete;
+
+const MarketingScreen = () => (
+  <Box>
+    <Text fontSize="24px" fontWeight="bold" mb="10px">
+      Welcome to Beerlot!
+    </Text>
+    <Text fontSize="16px" fontWeight="normal" mb="10px">
+      Please enter the beer name you want to search.
+    </Text>
+  </Box>
+);
