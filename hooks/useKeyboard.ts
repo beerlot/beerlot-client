c019836a9e71f8ff48300c34cc27b0a690b0d864
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-11-26 18:19:18 +0900

    chore: comments deleted

--- hooks/useKeyboard.ts
+++ hooks/useKeyboard.ts
@@ -1,6 +1,3 @@
-// make a hook that returns a function that can be used to add a keydown event listener
-// that will call the callback when the key is pressed
-
 import { AllKeyboardEvent } from "../interface/types";
 
 const useKeyboard = () => {
