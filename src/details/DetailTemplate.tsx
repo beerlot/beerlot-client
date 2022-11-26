95d1c7af5c8d0c4fad119d091d4ffed4d57770ee
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-11-27 01:05:24 +0900

    feat: Image section added

--- src/details/DetailTemplate.tsx
+++ src/details/DetailTemplate.tsx
@@ -1,3 +1,4 @@
+import { Box, Center, Image } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { LeftBackBeerNameRightHeart } from "../../common/headers/LeftBackBeerNameRightHeart";
 import { LeftBackTItle } from "../../common/headers/LeftBackTitle";
@@ -5,14 +6,24 @@ import { LeftBackTItle } from "../../common/headers/LeftBackTitle";
 export const DetailTemplate = () => {
   const [didPassStar, setDidPassStar] = useState(false);
   const beerName = "버드와이저"; // mock data
-
+  const MOCK_IMAGE_SRC = "https://picsum.photos/seed/picsum/200/300"; // mock data
   return (
     <>
+      {/* title */}
       {didPassStar ? (
         <LeftBackBeerNameRightHeart beerName={beerName} />
       ) : (
         <LeftBackTItle />
       )}
+      {/* image  */}
+      <Center pt="72px" w="full">
+        <Image
+          boxSize="320px"
+          src={MOCK_IMAGE_SRC}
+          fallbackSrc="https://via.placeholder.com/150"
+          alt="beer image"
+        />
+      </Center>
     </>
   );
 };
