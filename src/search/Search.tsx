24c8871b2531b14911ff9593c122986857338be3
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-12-19 16:04:49 +0900

    chore: border 10px removed

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -5,7 +5,7 @@ import SearchBarAutocomplete from "./SearchBarAutocomplete";
 
 const SearchTemplate = () => {
   return (
-    <VStack pt="70px" border="10px solid red" h="full" w="full">
+    <VStack pt="70px" h="full" w="full">
       <LeftBackTItle />
       <Box
         id="bg-dim"
