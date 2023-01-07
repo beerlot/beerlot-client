eae48eb9f14fd0f87606de9a23bf640da800a9b0
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-01-07 20:10:59 +0900

    feat: rating component icons changed

--- common/Rating.tsx
+++ common/Rating.tsx
@@ -1,6 +1,6 @@
 import {HStack, IconButton} from "@chakra-ui/react";
 import React from "react";
-import {OrangeStar} from "./custom-icons/customIcons";
+import {EmptyStar, FullStar} from "../public/svg";
 
 interface RatingProps {
   starSize?: number;
@@ -20,11 +20,16 @@ export const Rating: React.FC<RatingProps> = ({
       {[1, 2, 3, 4, 5].map((star) => {
         return (
           <IconButton
+            disabled={onClick ? false : true}
+            cursor="pointer"
+            _hover={{}}
+            _focus={{}}
             key={star}
+            bg="initial"
             aria-label="star"
-            as={OrangeStar}
+            boxShadow={"none"}
             fontSize={`${starSize}px`}
-            color={star <= rate ? "orange.200" : "white"}
+            as={star <= rate ? FullStar : EmptyStar}
             size={`${starSize}px`}
             onClick={onClick ? () => onClick(star) : undefined}
           />
