dc005adb59916de629970b8cebffcc581d9014f2
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-01-07 15:46:43 +0900

    feat: feed title synced

---
+++ common/headers/CenteredTitle.tsx
@@ -0,0 +1,21 @@
+import {Flex} from "@chakra-ui/react";
+import BeerlotTitle from "./BeerlotTitle";
+
+export const CenteredTitle = ({...props}) => {
+  return (
+    <Flex
+      {...props}
+      position="absolute"
+      top="0px"
+      right="0px"
+      left="0px"
+      py="16px"
+      px="20px"
+      justifyContent="center"
+      alignItems="center"
+      dropShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+    >
+      <BeerlotTitle />
+    </Flex>
+  );
+};
