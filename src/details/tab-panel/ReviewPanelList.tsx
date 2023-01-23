220b2fc31b7e04db86500ed7d517bb064c8ec170
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-01-23 17:52:07 +0900

    feat: info panel adding (wip)

--- src/details/tab-panel/ReviewPanelList.tsx
+++ src/details/tab-panel/ReviewPanelList.tsx
@@ -1,5 +1,28 @@
-import {Box} from "@chakra-ui/react";
+import {Box, Container, VStack, Text} from "@chakra-ui/react";
+import {BeerInfoHStack} from "./BasicPanelList";
 
 export const ReviewPanelList = () => {
-  return <Box bg="yellow.100">ReviewPanelList</Box>;
+  const reviewData = [];
+  return (
+    <Container px="8px">
+      <VStack px={"12px"} gap="10px" alignItems={"start"}>
+        <Box>
+          <Text textColor="black.100" as="span" textStyle={"h2_bold"}>
+            리뷰{" "}
+          </Text>
+          <Text textColor="orange.200" as="span" textStyle={"h2_bold"}>
+            {reviewData.length}
+          </Text>
+        </Box>
+        <BeerInfoHStack
+          label={"제보된 판매처"}
+          desc={"편의점"}
+          flexBasis={"81px"}
+        />
+        <BeerInfoHStack label={"평균 별점"} flexBasis={"81px"}>
+          <Box bg="yellow" w="20px" h="20px" />
+        </BeerInfoHStack>
+      </VStack>
+    </Container>
+  );
 };
