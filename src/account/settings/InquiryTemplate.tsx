307ea216a590668891b93e4a7c92120b6a226ee1
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-02-10 20:41:40 +0900

    chore: inquiry page added

--- src/account/settings/InquiryTemplate.tsx
+++ src/account/settings/InquiryTemplate.tsx
@@ -1,29 +1,38 @@
-import {Button, Container, Link, Text} from "@chakra-ui/react";
-import React from "react";
+import {Button, Center, Link, Text} from "@chakra-ui/react";
+import {useRouter} from "next/router";
+import {LeftBackRandom} from "../../../common/headers/LeftBackRandom";
 
 export const InquiryTemplate = () => {
-  // put it into environment variable
   const beerlotEmail = process.env.NEXT_PUBLIC_INQUIRY_EMAIL;
+  const router = useRouter();
 
+  const handleClickBack = () => {
+    router.back();
+  };
   return (
-    <Container centerContent border="1px solid">
-      <Text textColor="black.100">사용하시면서 문의사항이 생겼나요?🧐</Text>
-      <Text textColor="gray.300" mt="24px">
-        아래 버튼을 눌러 문의해주세요! 문의사항은 자세히 적어주실수록 좋아요 :)
-      </Text>
-      <Button
-        mt="24px"
-        as={Link}
-        href={`mailto:?subject=${beerlotEmail}`}
-        py={"10px"}
-        px={"73px"}
-        h="fit-content"
-        bg="gray.100"
-      >
-        <Text textColor={"black.100"} textStyle="h3_bold">
-          이메일로 문의하기
+    <>
+      <LeftBackRandom onClick={handleClickBack} title="문의하기" />
+      <Center flexDir={"column"} w="full" h="full">
+        <Text textColor="black.100">사용하시면서 문의사항이 생겼나요?🧐</Text>
+        <Text textColor="gray.300" textAlign={"center"} mt="24px">
+          아래 버튼을 눌러 문의해주세요! <br />
+          문의사항은 자세히 적어주실수록 좋아요 :)
         </Text>
-      </Button>
-    </Container>
+        <Button
+          _hover={{textDecoration: "none"}}
+          mt="24px"
+          as={Link}
+          href={`mailto:?subject=${beerlotEmail}`}
+          py={"10px"}
+          px={"73px"}
+          h="fit-content"
+          bg="gray.100"
+        >
+          <Text textColor={"black.100"} textStyle="h3_bold">
+            이메일로 문의하기
+          </Text>
+        </Button>
+      </Center>
+    </>
   );
 };
