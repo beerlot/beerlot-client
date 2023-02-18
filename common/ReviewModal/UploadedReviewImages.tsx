77bf25f711950b616f138313a7c8bcf45f389156
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-02-18 17:59:51 +0900

    feat: logic done, delete bug exists, dele button styling wip

--- common/ReviewModal/UploadedReviewImages.tsx
+++ common/ReviewModal/UploadedReviewImages.tsx
@@ -1,53 +1,116 @@
-import {Box, Button, HStack, Text} from "@chakra-ui/react";
-import React, {ChangeEvent, useState} from "react";
+import {CloseIcon} from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Image,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import {useRef, useState} from "react";
 import {ReviewStatic} from "../../interface/static";
 import {OrangeCamera} from "../../public/svg";
 
 export const UploadedReviewImages = () => {
-  const [attachedFile, setAttachedPhoto] = useState<File[]>([]);
+  const [imgFile, setImgFile] = useState<string[]>([]);
+  const imgRef = useRef<HTMLInputElement>(null);
 
-  // const deletePhoto = (targetId: string) => {
-  //   const newAttachedPhoto = attachedFile.filter((id) => id !== targetId);
-  //   setAttachedPhoto(newAttachedPhoto);
-  // };
+  const onClickDelete = (index: number) => {
+    const newImgFile = imgFile.filter((_, i) => i !== index);
+    setImgFile(newImgFile);
+  };
 
-  const onChangeFile = async (e: ChangeEvent<HTMLInputElement>) => {
-    //files는 있을수도 있고,없을수도 있기 때문에 옵셔널 체이닝을 사용해주셔야 합니다.
-    if (!e.target.files) return;
-    const Imagefile = e.target.files[0];
-    console.log("Imagefile", Imagefile);
-    //이미지 검증 함수를 끌어오겠습니다.
-    // const isValid = checkFileValidation(Imagefile);
-    //이미지 검증의 결과값에 따라 업로드를 진행하거나 함수를 종료합니다.
-    // if (!isValid) return;
-    setAttachedPhoto([...attachedFile, Imagefile]);
-    return Imagefile;
+  const handleChangeImage = () => {
+    if (imgFile.length >= ReviewStatic.numberOfMaxAttachedFile) return;
+    if (
+      !imgRef ||
+      !imgRef.current ||
+      !imgRef.current.files ||
+      imgRef.current.files.length === 0
+    )
+      return;
+    const file = imgRef.current.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    console.log("reader.result", reader.result);
+    reader.onloadend = () => {
+      if (typeof reader.result === "string") {
+        const newImgFile = [...imgFile, reader.result];
+        setImgFile(newImgFile);
+      }
+    };
   };
 
   return (
     <>
-      <Button
+      <FormControl>
+        <Button
+          htmlFor="attachFileInput"
+          as={FormLabel}
+          w="full"
+          bg="inherit"
+          _hover={{}}
+          border="1px solid"
+          borderColor="orange.200"
+          borderRadius="10px"
+          p="5px 10px"
+          aria-label="attach-photo"
+          gap="10px"
+          mt="0px"
+          disabled={imgFile.length >= ReviewStatic.numberOfMaxAttachedFile}
+          _notFirst={{marginInlineStart: "0px", marginTop: "0px"}}
+        >
+          <OrangeCamera />
+          <Text textStyle="h3" textColor="orange.200">
+            사진 첨부하기 ({imgFile.length}/
+            {ReviewStatic.numberOfMaxAttachedFile})
+          </Text>
+        </Button>
+        <Input
+          type="file"
+          accept="image/*"
+          id="attachFileInput"
+          onChange={handleChangeImage}
+          ref={imgRef}
+          disabled={imgFile.length >= ReviewStatic.numberOfMaxAttachedFile}
+          style={{display: "none"}}
+        />
+      </FormControl>
+
+      <HStack
+        display={imgFile.length > 0 ? "flex" : "none"}
+        overflowX="scroll"
         w="full"
-        bg="inherit"
-        _hover={{}}
-        border="1px solid"
-        borderColor="orange.200"
-        borderRadius="10px"
-        p="5px 10px"
-        aria-label="attach-photo"
-        gap="10px"
-        mt="0px"
-        _notFirst={{marginInlineStart: "0px", marginTop: "0px"}}
+        style={{marginTop: 0}}
       >
-        <OrangeCamera />
-        <Text textStyle="h3" textColor="orange.200">
-          사진 첨부하기 ({attachedFile.length}/
-          {ReviewStatic.numberOfMaxAttachedFile})
-        </Text>
-      </Button>
-      <HStack display={attachedFile.length > 0 ? "flex" : "none"}>
-        {attachedFile.map((item, index) => {
-          return <input key={index} type="file" onChange={onChangeFile} />;
+        {imgFile.map((fileSrc, index) => {
+          return (
+            <Box key={fileSrc} boxSize="80px" position="relative">
+              <Image
+                borderRadius={10}
+                src={fileSrc}
+                alt={"review pictures"}
+                w="full"
+                h="full"
+              />
+              <Button
+                w={"18px"}
+                h={"18px"}
+                p={0}
+                bg="black.200"
+                borderRadius={"full"}
+                position="absolute"
+                top={1}
+                right={1}
+                onClick={() => onClickDelete(index)}
+              >
+                <CloseIcon color="white.100" w={"4px"} h={"4px"} />
+              </Button>
+            </Box>
+          );
         })}
       </HStack>
     </>
