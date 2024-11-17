bfc1d6a7f9ea86e6f2e3b15e853f879187cf6762
tae100k <taedabomd@gmail.com>
2024-11-17 13:45:31 +0900

    feat: beer taste like button done

--- src/components/auth/sign-up/RecommendBeerCard.tsx
+++ src/components/auth/sign-up/RecommendBeerCard.tsx
@@ -9,8 +9,15 @@ import {
 } from '@components/shared/Card/BeerCardItem'
 import { Box, HStack } from '@chakra-ui/react'
 import { CommonBeerImage } from '@components/shared/CommonBeerImage/CommonBeerImage'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { BeerType } from '../../../../types/beer'
+import { LikeButton } from '@components/shared/LikeButton'
+import {
+  useBeerDislikeMutation,
+  useBeerLikeMutation,
+} from '../../../../hooks/query/useBeerLikeMutation'
+import { useRouter } from 'next/router'
+import Cookies from 'js-cookie'
 
 export const RecommendBeerCard = ({
   item,
@@ -20,42 +27,76 @@ export const RecommendBeerCard = ({
   onClick: () => void
   selected: undefined | boolean
   item: BeerType
-}) => (
-  <BeerCard
-    mt={1}
-    w='full'
-    borderColor={'orange.200'}
-    cursor='pointer'
-    onClick={onClick}
-    filter={selected ? 'drop-shadow(0px 8px 16px rgba(0, 0, 0, 0.3))' : 'none'}
-    bg={selected ? 'orange.200' : 'white.100'}
-  >
-    <BeerCardBody w='full' h='full' position={'relative'} border='orange.200'>
-      <Box position='relative' borderRadius={6}>
-        {item.image_url && (
-          <CommonBeerImage
-            src={item.image_url}
-            alt={item.name}
-            width='175px'
-            height='120px'
-            objectFit='cover'
-            style={{ borderRadius: '6px' }}
+}) => {
+  const [localLiked, setLocalLiked] = useState(false)
+  const accessToken = Cookies.get('beerlot-oauth-auth-request') ?? ''
+
+  const likeBeerMutation = useBeerLikeMutation(accessToken)
+  const dislikeBeerMutation = useBeerDislikeMutation(accessToken)
+
+  const handleClickBeerCard = () => {
+    handleClickLike()
+    onClick()
+  }
+
+  const handleClickLike = () => {
+    if (item.id === undefined) return
+    if (localLiked) {
+      dislikeBeerMutation.mutate(item.id)
+    } else {
+      likeBeerMutation.mutate(item.id)
+    }
+    setLocalLiked(!localLiked)
+  }
+
+  return (
+    <BeerCard
+      mt={1}
+      w='full'
+      borderColor={'orange.200'}
+      cursor='pointer'
+      onClick={handleClickBeerCard}
+      filter={
+        selected ? 'drop-shadow(0px 8px 16px rgba(0, 0, 0, 0.3))' : 'none'
+      }
+      bg={selected ? 'orange.200' : 'white.100'}
+    >
+      <BeerCardBody w='full' h='full' position={'relative'} border='orange.200'>
+        <Box position='relative' borderRadius={6}>
+          {item.image_url && (
+            <CommonBeerImage
+              src={item.image_url}
+              alt={item.name}
+              width='175px'
+              height='120px'
+              objectFit='cover'
+              style={{ borderRadius: '6px' }}
+            />
+          )}
+        </Box>
+        <Box position='absolute' top={0} right={0}>
+          <LikeButton
+            isLiked={localLiked}
+            onClick={handleClickLike}
+            h={7}
+            isControlled={true}
+            aria-label='like button'
           />
-        )}
-      </Box>
-    </BeerCardBody>
-    <BeerCardFooter>
-      <BeerNameText>{item.name}</BeerNameText>
-      <HStack>
-        <BeerCountryText borderRadius='full' country={item.origin_country} />
-        <BeerCategoryTag bg={selected ? 'white.100' : 'orange.200'}>
-          <BeerCategoryTagLabel
-            textColor={selected ? 'orange.200' : 'white.100'}
-          >
-            {item.category?.name}
-          </BeerCategoryTagLabel>
-        </BeerCategoryTag>
-      </HStack>
-    </BeerCardFooter>
-  </BeerCard>
-)
+        </Box>
+      </BeerCardBody>
+      <BeerCardFooter>
+        <BeerNameText>{item.name}</BeerNameText>
+        <HStack>
+          <BeerCountryText borderRadius='full' country={item.origin_country} />
+          <BeerCategoryTag bg={selected ? 'white.100' : 'orange.200'}>
+            <BeerCategoryTagLabel
+              textColor={selected ? 'orange.200' : 'white.100'}
+            >
+              {item.category?.name}
+            </BeerCategoryTagLabel>
+          </BeerCategoryTag>
+        </HStack>
+      </BeerCardFooter>
+    </BeerCard>
+  )
+}
--- src/components/shared/LikeButton.tsx
+++ src/components/shared/LikeButton.tsx
@@ -5,26 +5,31 @@ import React, { useState } from 'react'
 interface LikeButtonProps extends IconButtonProps {
   isLiked: boolean
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  isControlled?: boolean
 }
 
 export const LikeButton: React.FC<LikeButtonProps> = ({
   isLiked,
   onClick,
+  isControlled,
   ...props
 }) => {
   const [localLiked, setLocalLiked] = useState(isLiked)
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setLocalLiked(!localLiked)
+    if (!isControlled) {
+      setLocalLiked(!localLiked)
+    }
     onClick?.(e)
   }
 
+  const displayLiked = isControlled ? isLiked : localLiked
   return (
     <IconButton
       w='fit-content'
       minW='initial'
       onClick={handleClick}
-      icon={localLiked ? <FullHeart /> : <EmptyHeart />}
+      icon={displayLiked ? <FullHeart /> : <EmptyHeart />}
       cursor='pointer'
       fontSize={'28px'}
       _hover={{}}
