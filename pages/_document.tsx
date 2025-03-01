efc565327cb1a938e34c699e4fddada6b0d0c121
username <78027252+tae100k@users.noreply.github.com>
2025-03-01 14:41:23 +0900

    feat: _document updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -56,6 +56,7 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap'
             rel='stylesheet'
           />
+          <meta name="google-site-verification" content="QKz0iVRYbRCYxErp4h5sFoAQACcvxIPYU0bXWHD1lUU" />
         </Head>
         <body>
           <Main />
