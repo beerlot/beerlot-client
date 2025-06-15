9c8a3af93349a01a6a1abd7760208b1ee254c447
username <78027252+tae100k@users.noreply.github.com>
2025-06-15 16:45:27 +0900

    feat: add og:image meta tags for better social media sharing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,12 +62,15 @@ class MyDocument extends Document {
           <meta property="og:title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
           <meta property="og:description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
           <meta property="og:url" content="https://beerlot.site/" />
-          <meta property="og:image" content="https://beerlot.site/images/beerlot-preview-default-image.jpg" />
+          <meta property="og:image" content="https://www.beerlot.site/images/beerlot-preview-default-image.jpg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:image:type" content="image/jpeg" />
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
           <meta name="twitter:description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
-          <meta name="twitter:image" content="https://beerlot.site/images/beerlot-preview-default-image.jpg" />
+          <meta name="twitter:image" content="https://www.beerlot.site/images/beerlot-preview-default-image.jpg" />
         </Head>
         <body>
           <Main />
