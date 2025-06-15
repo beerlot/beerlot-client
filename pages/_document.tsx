75f66121cf5431a6d7e2c92abf98b4cfd7f3a762
username <78027252+tae100k@users.noreply.github.com>
2025-06-15 16:52:39 +0900

    feat: update meta tags for social media preview

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,19 +58,26 @@ class MyDocument extends Document {
             rel='stylesheet'
           />
           <meta name="google-site-verification" content="QKz0iVRYbRCYxErp4h5sFoAQACcvxIPYU0bXWHD1lUU" />
-          <meta property="og:site_name" content="비어랏" />
+          
+          {/* Primary Meta Tags */}
+          <title>오늘도 수고한 당신, 맥주 한 잔 비어랏!</title>
+          <meta name="title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
+          <meta name="description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
+          
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://beerlot.site/" />
           <meta property="og:title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
           <meta property="og:description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
-          <meta property="og:url" content="https://beerlot.site/" />
           <meta property="og:image" content="https://www.beerlot.site/images/beerlot-preview-default-image.jpg" />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta property="og:image:type" content="image/jpeg" />
-          <meta property="og:type" content="website" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
-          <meta name="twitter:description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
-          <meta name="twitter:image" content="https://www.beerlot.site/images/beerlot-preview-default-image.jpg" />
+          <meta property="og:site_name" content="비어랏" />
+          
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://beerlot.site/" />
+          <meta property="twitter:title" content="오늘도 수고한 당신, 맥주 한 잔 비어랏!" />
+          <meta property="twitter:description" content="맥주 검색, 추천, 기록까지 한 번에 – 오늘 마실 맥주, 비어랏에서 골라요." />
+          <meta property="twitter:image" content="https://www.beerlot.site/images/beerlot-preview-default-image.jpg" />
         </Head>
         <body>
           <Main />
